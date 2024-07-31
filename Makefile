eb5576d6c707b4db4f3e2881dd42af1f6d23cefb
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-07-31 11:22:58 -0400

    continuation for assignment

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 #	'ensure that formatted text option in draw.io is disabled everywhere'
 
-SRC=rt0d.rt
-#SRC=test.rt
+#SRC=rt0d.rt
+SRC=test.rt
 D2J=./das2json/mac/das2json
 
-test_python:
-	(make -s all >rtpy0d.py ; python3 rtpy0d.py)
+# test_python:
+# 	(make -s all >rtpy0d.py ; python3 rtpy0d.py)
 
 all:
 	${D2J} t2t.drawio
--- support.js
+++ support.js
@@ -1,7 +1,11 @@
 _ = {
     fname_stack : [],
-    fname : function () { return _.fname_stack [0]; },
+    continuation_stack : [],
+    fname : function () { return _.fname_stack [_.fname_stack.length - 1]; },
     push_fname : function (s) { _.fname_stack.push (s); },
-    pop_fname : function () { _.fname_stack.pop (); return ""; }
+    pop_fname : function () { _.fname_stack.pop (); return ""; },
+    push_continuation : function (s) { _.continuation_stack.push (s); },
+    pop_continuation : function () { _.continuation_stack.pop (); return ""; },
+    continuation : function () { return _.continuation_stack[_.continuation_stack.length -1]; }    
 }
 ,
