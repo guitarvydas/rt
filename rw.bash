265a7631409756e72024fd944a42f96ba68310ef
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-08-21 21:16:04 -0400

    rt0d.new.rt alpha

--- rw.bash
+++ rw.bash
@@ -1,1 +1,2 @@
-./rw1.bash | ./rw2.bash | ./rw3.bash
+./rw1.bash | ./rw2.bash | ./rw3.bash | ./rw4.bash
+
--- rw4.bash
+++ rw4.bash
@@ -1,2 +1,2 @@
-node t4t.mjs <mutate.t2t >edit4.mjs
+node t2t.mjs <mutate.t2t >edit4.mjs
 node edit4.mjs
