a70eef6b51f068649b63f5557f8099a43df0524a
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-10-01 05:46:22 -0400

    right quotes unicoded

--- generated-py0d.py
+++ generated-py0d.py
@@ -2,13 +2,13 @@
 
 class  Connector :
     def __init__ (self):
-        self. direction  =  None # ❲down❳, ❲across❳, ❲up❳, ❲through❳
+        self. direction  =  None # down, across, up, through
         self. sender  =  None
         self. receiver  =  None 
 
 def  raw_datum_string  ( d ):
     set_environment  ( root_project , root_0D )
-    # ❲get❳ ❲entrypoint❳ ❲container❳
+    # get entrypoint container
     return  bytearray  ( d . data ,"UTF_8")
 
 
