8a44b3298bc9ca9f3e39305d8059fa00381b5771
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-10-03 07:22:53 -0400

    minor indentation edits

--- generated-cl0d.lisp
+++ generated-cl0d.lisp
@@ -1,32 +1,48 @@
 
-Line 93, col 1:
-92 |
-> 93 | ****
-^
-94 |
-Expected "}"
 
-function t2t_phase2 (grammr, sem, scn) {
-let parser = ohm.grammar (grammr);
-let cst = parser.match (src);
-if (cst.succeeded ()) {
-let cstSemantics = parser.createSemantics ();
-cstSemantics.addOperation ('rwr', sem);
-var generated_code = cstSemantics (cst).rwr ();
-return generated_code;
-} else {
-return cst.message;
-}
-}
+(defun clone_datum_bang ( d)
+  (return-from undefined (new_datum_bang )))
+(defun Message ( port datum)
+  (list
+    (cons 'port  port)
+    (cons 'datum  datum) ))
 
-t2t_phase2 (dslGrammar, rewrite_js, src);
-
-SyntaxError: Unexpected number
-at file:///Users/paultarvydas/projects/rt/t2t.mjs:832:21
-at ModuleJob.run (node:internal/modules/esm/module_job:222:25)
-at async ModuleLoader.import (node:internal/modules/esm/loader:323:24)
-at async loadESM (node:internal/process/esm_loader:28:7)
-at async handleMainPromise (node:internal/modules/run_main:120:12)
+(defun clone_port ( s)
+  (return-from undefined (clone_string   s)))
+#|  Utility for making a `Message`. Used to safely “seed“ messages |#
+#|  entering the very top of a network. |#
+(defun make_message ( port datum)
+  (let (( p (clone_string   port)))
+    (let (( m (Message  :port  p :datum ((assoc 'clone  datum) ))))
+      (return-from undefined  m))))
+(defun xyz ()
+  (cond 
+    ( x
+        (setf  a  1))
+    ( y
+        (setf  a  2))
+    (t
+        (setf  a  3))))
+(defun low_level_read_text_file_instantiate ( reg owner name template_data)
+  (let (( name_with_id (gensym   "Low Level Read Text File")))
+    (return-from undefined (make_leaf   name_with_id  owner  nil  low_level_read_text_file_handler))))
+(defun low_level_read_text_file_handler ( eh msg)
+  (let (( fname ((assoc 'srepr (assoc 'datum  msg)) )))
+    ;; read text from a named file fname, send the text out on port "" else send error info on port "✗"
+    ;; given eh and msg if needed
+    (handler-bind ((error #'(lambda (condition) (send_string eh "✗" (format nil "~&~A~&" condition)))))
+      (with-open-file (stream fname)
+        (let ((contents (make-string (file-length stream))))
+          (read-sequence contents stream)
+          (send_string eh "" contents))))
+    ))
+(defun read_and_convert_json_file ( filename)
+  ;; read json from a named file and convert it into internal form (a tree of routings)
+  ;; return the routings from the function or print an error message and return nil
+  (handler-bind ((error #'(lambda (condition) nil)))
+    (with-open-file (json-stream "~/projects/rtlarson/eyeballs.json" :direction :input)
+      (json:decode-json json-stream)))
+  )
 
 
 
