ec3b894b1f7cd8834962ab5f1aae74853d57407a
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-10-16 22:54:31 -0400

    line? revamped up to emitPython

---
+++ generated-py0d.py
@@ -0,0 +1,13 @@
+
+
+def clone_datum_string (d):⎩2⎭
+    d = new_datum_string ( d.data)⎩3⎭
+    return  d⎩4⎭
+⎩5⎭#  test comment ⎩6⎭
+def f (x):⎩7⎭
+    x [ 77]  ***  error - LHS must be a single identifier for defsynonym  ***  =  42⎩8⎭
+⎩9⎭
+
+
+
+
