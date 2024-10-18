ee7d1601e28ef94c8ebdc1294b2494fb72795460
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-10-18 06:59:22 -0400

    blank line containing only line number

--- generated-py0d.py
+++ generated-py0d.py
@@ -3,10 +3,10 @@
 def clone_datum_string (d):#line 1
     d [ 88]  >>>  error - LHS must be a single identifier for defsynonym  <<<  = new_datum_string ( d.data)#line 2
     return  d#line 3
-#line 4#  test comment #line 5
-def f (x):#line 6
-    x [ 99]  >>>  error - LHS must be a single identifier for defsynonym  <<<  =  42#line 7
-#line 8
+#line 4#line 5#  test comment #line 6
+def f (x):#line 7
+    x [ 99]  >>>  error - LHS must be a single identifier for defsynonym  <<<  =  42#line 8
+#line 9
 
 
 
