5397ddd00d03389446a39a4a9ea6aa386ec434d1
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-10-19 09:51:06 -0400

    dict with lines for test.rt

--- generated-py0d.py
+++ generated-py0d.py
@@ -1,37 +1,49 @@
 
 
-def f ():                                                                       #line 1
+def make_Routing_Descriptor (action,component,port,message):                    #line 1
 
-    return [ before_state ==  "idle" and  after_state!= "idle",                 #line 2
-    before_state!= "idle" and  after_state!= "idle",                            #line 3
-    before_state!= "idle" and  after_state ==  "idle"]                          #line 4
-                                                                                #line 5
+    return {                                                                    #line 2
+    "action": action,                                                           #line 3
+    "component": component,                                                     #line 4
+    "port": port,                                                               #line 5
+    "message": message                                                          #line 6
+    }                                                                           #line 7
+                                                                                #line 8
 
-                                                                                #line 6
+                                                                                #line 9
 
-def fmt_inout (desc,indent):                                                    #line 7
+def f ():                                                                       #line 10
 
-    s ( "\n",                                                                   #line 8
-    x)                                                                          #line 9
-                                                                                #line 10
-
-                                                                                #line 11
-
-def clone_datum_string (d):                                                     #line 12
-
-    d = new_datum_string ( d.data)                                              #line 13
+    return [ before_state ==  "idle" and  after_state!= "idle",                 #line 11
+    before_state!= "idle" and  after_state!= "idle",                            #line 12
+    before_state!= "idle" and  after_state ==  "idle"]                          #line 13
                                                                                 #line 14
 
-    return  d                                                                   #line 15
-                                                                                #line 16
+                                                                                #line 15
 
-                                                                                #line 17
-#  test comment                                                                 #line 18
+def fmt_inout (desc,indent):                                                    #line 16
 
-def f (x):                                                                      #line 19
+    s ( "\n",                                                                   #line 17
+    x)                                                                          #line 18
+                                                                                #line 19
 
-    x =  42                                                                     #line 20
-                                                                                #line 21
+                                                                                #line 20
+
+def clone_datum_string (d):                                                     #line 21
+
+    d = new_datum_string ( d.data)                                              #line 22
+                                                                                #line 23
+
+    return  d                                                                   #line 24
+                                                                                #line 25
+
+                                                                                #line 26
+#  test comment                                                                 #line 27
+
+def f (x):                                                                      #line 28
+
+    x =  42                                                                     #line 29
+                                                                                #line 30
 
 
 
