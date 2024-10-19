20e0ff761a7caa6d35af85682c304a091275138b
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-10-19 07:39:29 -0400

    line for test.rt (error version in test.2.rt)

--- generated-py0d.py
+++ generated-py0d.py
@@ -1,7 +1,28 @@
 
 
-def fmt_inout (desc,indent):#line 1
-    s ( "\n",#line 2 x)#line 3#line 4
+def fmt_inout (desc,indent):                                                    #line 1
+
+    s ( "\n",                                                                   #line 2
+    x)                                                                          #line 3
+                                                                                #line 4
+
+                                                                                #line 5
+
+def clone_datum_string (d):                                                     #line 6
+
+    d = new_datum_string ( d.data)                                              #line 7
+
+    return  d                                                                   #line 8
+                                                                                #line 9
+
+                                                                                #line 10
+#  test comment                                                                 #line 11
+
+def f (x):                                                                      #line 12
+
+    x =  42                                                                     #line 13
+                                                                                #line 14
+
 
 
 
