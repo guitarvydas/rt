ec1e5ddafd1f656accd77bd7e6bf0c9a4d249d06
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-10-21 23:10:31 -0400

    "(not" missing close paren

--- generated.a.lisp
+++ generated.a.lisp
@@ -2,13 +2,11 @@
 (load "~/quicklisp/setup.lisp")
 (ql:quickload :cl-json)
 
-(defun f (&optional )                                                                                                   #|line 1|#
-
-    (let ((x (cdr (assoc '(cdr (assoc ' empty  inq))  child))))                                                         #|line 2|#
-
-        (let ((x (funcall (cdr (assoc '(cdr (assoc ' empty  inq))  child)) ))))                                         #|line 3|#
-      )                                                                                                                 #|line 4|#
-
+(defun attempt_tick (&optional  parent  eh)                                                                             #|line 1|#
+    (cond
+      ((not (equal  (cdr (assoc ' state  eh))  "idle"))                                                                 #|line 2|#
+            (funcall  force_tick    parent   eh )                                                                       #|line 3|#
+        ))                                                                                                              #|line 4|#
 )
 
 
