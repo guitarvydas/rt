792eccc915e718bb35cbbdcf3609aa1a1c422577
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-10-22 06:23:27 -0400

    Formal_defaultvalue corrected

--- generated.lisp
+++ generated.lisp
@@ -2,8 +2,8 @@
 (load "~/quicklisp/setup.lisp")
 (ql:quickload :cl-json)
 
-(defun f (&optional )                                                                                                   #|line 1|#
-    (let ((cmd (funcall (cdr (assoc ' strip  (subseq  name 1))) ))))                                                    #|line 2|##|line 3|#
+(defun log_inout_recursively (&optional  container  component  in_message  (out_messages  nil))                         #|line 1|#
+    (let ((rest  (cdr  out_messages))))                                                                                 #|line 2|##|line 3|#
 )
 
 
