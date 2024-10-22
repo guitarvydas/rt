b18c1063d70e722cdeb4b76344054fa965246ddd
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-10-22 05:50:01 -0400

    pmacro moved to Atom

--- generated.lisp
+++ generated.lisp
@@ -2,11 +2,8 @@
 (load "~/quicklisp/setup.lisp")
 (ql:quickload :cl-json)
 
-(defun attempt_tick (&optional  parent  eh)                                                                             #|line 1|#
-    (cond
-      ((not (equal  (cdr (assoc ' state  eh))  "idle"))                                                                 #|line 2|#
-            (funcall  force_tick    parent   eh )                                                                       #|line 3|#
-        ))                                                                                                              #|line 4|#
+(defun f (&optional )                                                                                                   #|line 1|#
+    (let ((cmd (funcall (cdr (assoc ' strip  (subseq  name 1))) ))))                                                    #|line 2|##|line 3|#
 )
 
 
