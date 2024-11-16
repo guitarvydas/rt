829918d471bd159904d900824486bf6947d805fe
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-11-16 04:25:35 -0500

    generate-py-chunk.bash

--- rebuild.bash
+++ rebuild.bash
@@ -11,14 +11,8 @@ D2J=./das2json/mac/das2json
 echo '#77 CL test successful' >generated.lisp
 echo '#77 line 2 CL test successful' >>generated.lisp
 ${D2J} rt2py.drawio
-python3 main.py . 0D/python ${SRC}.a main rt2py.drawio.json >generated.a.py
-python3 mvline.py generated.a.py 60 >/tmp/generated.a.py
-mv /tmp/generated.a.py ./generated.a.py
-python3 errcheck.py generated.a.py
-python3 main.py . 0D/python ${SRC}.b main rt2py.drawio.json >generated.b.py
-python3 mvline.py generated.b.py 60 >/tmp/generated.b.py
-mv /tmp/generated.b.py ./generated.b.py
-python3 errcheck.py generated.b.py
+./generate-py-chunk.bash ${SRC}.a generated.a.py
+./generate-py-chunk.bash ${SRC}.b generated.b.py
 cat generated.a.py generated.b.py >generated.py
 ${D2J} rt2cl.drawio
 ./generate-cl-chunk.bash ${SRC}.a generated.a.lisp
