3b0de0181b8a63cf2d2491660dca3c31e030e0ac
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-12-21 12:37:14 -0500

    add switch1* and latch

--- 0d.rt.b
+++ 0d.rt.b
@@ -604,6 +604,58 @@ defn fakepipename_handler (eh, msg) {
 }
 
 
+defobj Switch1star_Instance_Data () {
+        • state ⇐ “1”
+}
+
+defn switch1star_instantiate (reg, owner, name, template_data) {
+    name_with_id ≡ gensymbol (“switch1*”)
+    instp ≡ #fresh (Switch1star_Instance_Data)
+    return make_leaf (name_with_id, owner, instp, ↪︎switch1star_handler)
+}
+
+defn switch1star_handler (eh, msg) {
+    deftemp inst ⇐ eh.instance_data
+    whichOutput ≡ inst.state
+    if “” = msg.port {
+	if “1” = whichOutput {
+	    forward (eh, “1*”, msg.datum.v)
+	    inst.state ⇐ “*”
+	} elif “*” = whichOutput {
+	    forward (eh, “*”, msg.datum.v)
+	} else {
+	    send (eh, “✗”, “internal error bad state in switch1*”, msg)
+	}
+    } elif “reset” = msg.port {
+	    inst.state ⇐ “1”
+    } else {
+        send (eh, “✗”, “internal error bad message for switch1*”, msg)
+    }
+}
+
+defobj Latch_Instance_Data () {
+        • datum ⇐ ϕ
+}
+
+defn latch_instantiate (reg, owner, name, template_data) {
+    name_with_id ≡ gensymbol (“latch”)
+    instp ≡ #fresh (Latch_Instance_Data)
+    return make_leaf (name_with_id, owner, instp, ↪︎latch_handler)
+}
+
+defn latch_handler (eh, msg) {
+    deftemp inst ⇐ eh.instance_data
+    whichOutput ≡ inst.state
+    if “” = msg.port {
+        inst.datum ⇐ msg.datum
+    } elif “release” = msg.port {
+        deftemp d ⇐ inst.datum
+        send (eh, “”, d, msg)
+    } else {
+        send (eh, “✗”, “internal error bad message for latch”, msg)
+    }
+}
+
 ⌈ all of the the built_in leaves are listed here⌉
 ⌈ future: refactor this such that programmers can pick and choose which (lumps of) builtins are used in a specific project⌉
 
@@ -619,6 +671,8 @@ defn initialize_stock_components (reg) {
 
     register_component (reg, mkTemplate ( “syncfilewrite”, ϕ, ↪︎syncfilewrite_instantiate))
     register_component (reg, mkTemplate ( “stringconcat”, ϕ, ↪︎stringconcat_instantiate))
+    register_component (reg, mkTemplate ( “switch1*”, ϕ, ↪︎switch1star_instantiate))
+    register_component (reg, mkTemplate ( “latch”, ϕ, ↪︎latch_instantiate))
        ⌈ for fakepipe⌉
     register_component (reg, mkTemplate ( “fakepipename”, ϕ, ↪︎fakepipename_instantiate))
 }
--- generated.mjs
+++ generated.mjs
@@ -1021,81 +1021,138 @@ function fakepipename_handler (eh,msg) {               /* line 600 *//* line 601
     send_string ( eh, "", `${ "/tmp/fakepipe"}${ rand}` , msg)/* line 603 *//* line 604 *//* line 605 */
 }
                                                        /* line 606 */
-/*  all of the the built_in leaves are listed here */  /* line 607 */
-/*  future: refactor this such that programmers can pick and choose which (lumps of) builtins are used in a specific project *//* line 608 *//* line 609 */
-function initialize_stock_components (reg) {           /* line 610 */
-    register_component ( reg,mkTemplate ( "1then2", null, deracer_instantiate))/* line 611 */
-    register_component ( reg,mkTemplate ( "?A", null, probeA_instantiate))/* line 612 */
-    register_component ( reg,mkTemplate ( "?B", null, probeB_instantiate))/* line 613 */
-    register_component ( reg,mkTemplate ( "?C", null, probeC_instantiate))/* line 614 */
-    register_component ( reg,mkTemplate ( "trash", null, trash_instantiate))/* line 615 *//* line 616 */
-    register_component ( reg,mkTemplate ( "Low Level Read Text File", null, low_level_read_text_file_instantiate))/* line 617 */
-    register_component ( reg,mkTemplate ( "Ensure String Datum", null, ensure_string_datum_instantiate))/* line 618 *//* line 619 */
-    register_component ( reg,mkTemplate ( "syncfilewrite", null, syncfilewrite_instantiate))/* line 620 */
-    register_component ( reg,mkTemplate ( "stringconcat", null, stringconcat_instantiate))/* line 621 */
-    /*  for fakepipe */                                /* line 622 */
-    register_component ( reg,mkTemplate ( "fakepipename", null, fakepipename_instantiate))/* line 623 *//* line 624 *//* line 625 */
+class Switch1star_Instance_Data {
+  constructor () {                                     /* line 607 */
+
+    this.state =  "1";                                 /* line 608 *//* line 609 */
+  }
+}
+                                                       /* line 610 */
+function switch1star_instantiate (reg,owner,name,template_data) {/* line 611 */
+    let name_with_id = gensymbol ( "switch1*")         /* line 612 */;
+    let instp =  new Switch1star_Instance_Data ();     /* line 613 */;
+    return make_leaf ( name_with_id, owner, instp, switch1star_handler)/* line 614 */;/* line 615 *//* line 616 */
 }
 
-function argv () {                                     /* line 626 */
-    return  command_line_argv                          /* line 627 */;/* line 628 *//* line 629 */
+function switch1star_handler (eh,msg) {                /* line 617 */
+    let  inst =  eh.instance_data;                     /* line 618 */
+    let whichOutput =  inst.state;                     /* line 619 */
+    if ( "" ==  msg.port) {                            /* line 620 */
+      if ( "1" ==  whichOutput) {                      /* line 621 */
+        forward ( eh, "1*", msg.datum.v)               /* line 622 */
+        inst.state =  "*";                             /* line 623 */}
+      else if ( "*" ==  whichOutput) {                 /* line 624 */
+        forward ( eh, "*", msg.datum.v)                /* line 625 */}
+      else {                                           /* line 626 */
+        send ( eh, "✗", "internal error bad state in switch1*", msg)/* line 627 *//* line 628 */}}
+    else if ( "reset" ==  msg.port) {                  /* line 629 */
+      inst.state =  "1";                               /* line 630 */}
+    else {                                             /* line 631 */
+      send ( eh, "✗", "internal error bad message for switch1*", msg)/* line 632 *//* line 633 */}/* line 634 *//* line 635 */
 }
 
-function initialize () {                               /* line 630 */
-    let root_of_project =  command_line_argv[ 1]       /* line 631 */;
-    let root_of_0D =  command_line_argv[ 2]            /* line 632 */;
-    let arg =  command_line_argv[ 3]                   /* line 633 */;
-    let main_container_name =  command_line_argv[ 4]   /* line 634 */;
-    let diagram_names =  command_line_argv.splice ( 5) /* line 635 */;
-    let palette = initialize_component_palette ( root_of_project, root_of_0D, diagram_names)/* line 636 */;
-    return [ palette,[ root_of_project, root_of_0D, main_container_name, diagram_names, arg]];/* line 637 *//* line 638 *//* line 639 */
+class Latch_Instance_Data {
+  constructor () {                                     /* line 636 */
+
+    this.datum =  null;                                /* line 637 *//* line 638 */
+  }
+}
+                                                       /* line 639 */
+function latch_instantiate (reg,owner,name,template_data) {/* line 640 */
+    let name_with_id = gensymbol ( "latch")            /* line 641 */;
+    let instp =  new Latch_Instance_Data ();           /* line 642 */;
+    return make_leaf ( name_with_id, owner, instp, latch_handler)/* line 643 */;/* line 644 *//* line 645 */
+}
+
+function latch_handler (eh,msg) {                      /* line 646 */
+    let  inst =  eh.instance_data;                     /* line 647 */
+    let whichOutput =  inst.state;                     /* line 648 */
+    if ( "" ==  msg.port) {                            /* line 649 */
+      inst.datum =  msg.datum;                         /* line 650 */}
+    else if ( "release" ==  msg.port) {                /* line 651 */
+      let  d =  inst.datum;                            /* line 652 */
+      send ( eh, "", d, msg)                           /* line 653 */}
+    else {                                             /* line 654 */
+      send ( eh, "✗", "internal error bad message for latch", msg)/* line 655 *//* line 656 */}/* line 657 *//* line 658 */
+}
+
+/*  all of the the built_in leaves are listed here */  /* line 659 */
+/*  future: refactor this such that programmers can pick and choose which (lumps of) builtins are used in a specific project *//* line 660 *//* line 661 */
+function initialize_stock_components (reg) {           /* line 662 */
+    register_component ( reg,mkTemplate ( "1then2", null, deracer_instantiate))/* line 663 */
+    register_component ( reg,mkTemplate ( "?A", null, probeA_instantiate))/* line 664 */
+    register_component ( reg,mkTemplate ( "?B", null, probeB_instantiate))/* line 665 */
+    register_component ( reg,mkTemplate ( "?C", null, probeC_instantiate))/* line 666 */
+    register_component ( reg,mkTemplate ( "trash", null, trash_instantiate))/* line 667 *//* line 668 */
+    register_component ( reg,mkTemplate ( "Low Level Read Text File", null, low_level_read_text_file_instantiate))/* line 669 */
+    register_component ( reg,mkTemplate ( "Ensure String Datum", null, ensure_string_datum_instantiate))/* line 670 *//* line 671 */
+    register_component ( reg,mkTemplate ( "syncfilewrite", null, syncfilewrite_instantiate))/* line 672 */
+    register_component ( reg,mkTemplate ( "stringconcat", null, stringconcat_instantiate))/* line 673 */
+    register_component ( reg,mkTemplate ( "switch1*", null, switch1star_instantiate))/* line 674 */
+    register_component ( reg,mkTemplate ( "latch", null, latch_instantiate))/* line 675 */
+    /*  for fakepipe */                                /* line 676 */
+    register_component ( reg,mkTemplate ( "fakepipename", null, fakepipename_instantiate))/* line 677 *//* line 678 *//* line 679 */
+}
+
+function argv () {                                     /* line 680 */
+    return  command_line_argv                          /* line 681 */;/* line 682 *//* line 683 */
+}
+
+function initialize () {                               /* line 684 */
+    let root_of_project =  command_line_argv[ 1]       /* line 685 */;
+    let root_of_0D =  command_line_argv[ 2]            /* line 686 */;
+    let arg =  command_line_argv[ 3]                   /* line 687 */;
+    let main_container_name =  command_line_argv[ 4]   /* line 688 */;
+    let diagram_names =  command_line_argv.splice ( 5) /* line 689 */;
+    let palette = initialize_component_palette ( root_of_project, root_of_0D, diagram_names)/* line 690 */;
+    return [ palette,[ root_of_project, root_of_0D, main_container_name, diagram_names, arg]];/* line 691 *//* line 692 *//* line 693 */
 }
 
 function start (palette,env) {
-    start_helper ( palette, env, false)                /* line 640 */
+    start_helper ( palette, env, false)                /* line 694 */
 }
 
 function start_show_all (palette,env) {
-    start_helper ( palette, env, true)                 /* line 641 */
+    start_helper ( palette, env, true)                 /* line 695 */
 }
 
-function start_helper (palette,env,show_all_outputs) { /* line 642 */
-    let root_of_project =  env [ 0];                   /* line 643 */
-    let root_of_0D =  env [ 1];                        /* line 644 */
-    let main_container_name =  env [ 2];               /* line 645 */
-    let diagram_names =  env [ 3];                     /* line 646 */
-    let arg =  env [ 4];                               /* line 647 */
-    set_environment ( root_of_project, root_of_0D)     /* line 648 */
-    /*  get entrypoint container */                    /* line 649 */
-    let  main_container = get_component_instance ( palette, main_container_name, null)/* line 650 */;
-    if ( null ==  main_container) {                    /* line 651 */
-      load_error ( `${ "Couldn't find container with page name /"}${ `${ main_container_name}${ `${ "/ in files "}${ `${`${ diagram_names}`}${ " (check tab names, or disable compression?)"}` }` }` }` )/* line 655 *//* line 656 */}
-    if ((!  load_errors)) {                            /* line 657 */
-      let  marg = new_datum_string ( arg)              /* line 658 */;
-      let  msg = make_message ( "", marg)              /* line 659 */;
-      inject ( main_container, msg)                    /* line 660 */
-      if ( show_all_outputs) {                         /* line 661 */
-        dump_outputs ( main_container)                 /* line 662 */}
-      else {                                           /* line 663 */
-        print_error_maybe ( main_container)            /* line 664 */
-        let outp = fetch_first_output ( main_container, "")/* line 665 */;
-        if ( null ==  outp) {                          /* line 666 */
-          console.log ( "«««no outputs»»»)");          /* line 667 */}
-        else {                                         /* line 668 */
-          print_specific_output ( main_container, "")  /* line 669 *//* line 670 */}/* line 671 */}
-      if ( show_all_outputs) {                         /* line 672 */
-        console.log ( "--- done ---");                 /* line 673 *//* line 674 */}/* line 675 */}/* line 676 *//* line 677 */
+function start_helper (palette,env,show_all_outputs) { /* line 696 */
+    let root_of_project =  env [ 0];                   /* line 697 */
+    let root_of_0D =  env [ 1];                        /* line 698 */
+    let main_container_name =  env [ 2];               /* line 699 */
+    let diagram_names =  env [ 3];                     /* line 700 */
+    let arg =  env [ 4];                               /* line 701 */
+    set_environment ( root_of_project, root_of_0D)     /* line 702 */
+    /*  get entrypoint container */                    /* line 703 */
+    let  main_container = get_component_instance ( palette, main_container_name, null)/* line 704 */;
+    if ( null ==  main_container) {                    /* line 705 */
+      load_error ( `${ "Couldn't find container with page name /"}${ `${ main_container_name}${ `${ "/ in files "}${ `${`${ diagram_names}`}${ " (check tab names, or disable compression?)"}` }` }` }` )/* line 709 *//* line 710 */}
+    if ((!  load_errors)) {                            /* line 711 */
+      let  marg = new_datum_string ( arg)              /* line 712 */;
+      let  msg = make_message ( "", marg)              /* line 713 */;
+      inject ( main_container, msg)                    /* line 714 */
+      if ( show_all_outputs) {                         /* line 715 */
+        dump_outputs ( main_container)                 /* line 716 */}
+      else {                                           /* line 717 */
+        print_error_maybe ( main_container)            /* line 718 */
+        let outp = fetch_first_output ( main_container, "")/* line 719 */;
+        if ( null ==  outp) {                          /* line 720 */
+          console.log ( "«««no outputs»»»)");          /* line 721 */}
+        else {                                         /* line 722 */
+          print_specific_output ( main_container, "")  /* line 723 *//* line 724 */}/* line 725 */}
+      if ( show_all_outputs) {                         /* line 726 */
+        console.log ( "--- done ---");                 /* line 727 *//* line 728 */}/* line 729 */}/* line 730 *//* line 731 */
 }
-                                                       /* line 678 */
-/*  utility functions  */                              /* line 679 */
-function send_int (eh,port,i,causing_message) {        /* line 680 */
-    let datum = new_datum_string (`${ i}`)             /* line 681 */;
-    send ( eh, port, datum, causing_message)           /* line 682 *//* line 683 *//* line 684 */
+                                                       /* line 732 */
+/*  utility functions  */                              /* line 733 */
+function send_int (eh,port,i,causing_message) {        /* line 734 */
+    let datum = new_datum_string (`${ i}`)             /* line 735 */;
+    send ( eh, port, datum, causing_message)           /* line 736 *//* line 737 *//* line 738 */
 }
 
-function send_bang (eh,port,causing_message) {         /* line 685 */
-    let datum = new_datum_bang ();                     /* line 686 */
-    send ( eh, port, datum, causing_message)           /* line 687 *//* line 688 */
+function send_bang (eh,port,causing_message) {         /* line 739 */
+    let datum = new_datum_bang ();                     /* line 740 */
+    send ( eh, port, datum, causing_message)           /* line 741 *//* line 742 */
 }
 
 
